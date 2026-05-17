8ed1108bb7d0b6a70ea3f7b9a627a807d7cce00b
MiguelScofielD <antdp408@gmail.com>
2026-05-17 15:25:08 -0300

    Frontend separated from AI worker

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 import os
 import fitz
-import soundfile as sf
+import requests
+import uuid
+from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+
 
 from flask import Flask, render_template, request, send_file
 
-from kokoro import KPipeline
+load_dotenv()
+AI_API_URL = os.getenv("AI_API_URL")
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
 GENERATED_FOLDER = "generated"
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(GENERATED_FOLDER, exist_ok=True)
+
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["GENERATED_FOLDER"] = GENERATED_FOLDER
-
-# inicia pipeline
-pipeline = KPipeline(lang_code="p")
 
 
 @app.route("/")
@@ -26,33 +30,50 @@ def index():
 
 @app.route("/convert", methods=["POST"])
 def convert():
+
     if "pdf" not in request.files:
         return "No file selected"
 
     file = request.files["pdf"]
+
     if file.filename == "":
         return "No file selected"
 
-    filename = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+    # salva pdf
+    safe_name = secure_filename(file.filename)
+
+    unique_filename = f"{uuid.uuid4()}_{safe_name}"
+
+    filename = os.path.join(app.config["UPLOAD_FOLDER"], unique_filename)
+
     file.save(filename)
 
-    # Open the PDF and extract text
+    # extrai texto
     doc = fitz.open(filename)
+
     text = ""
+
     for page in doc:
         text += page.get_text()
+
     # limita texto inicialmente
-    text = text[:500]
+    # text = text[:1000]
 
-    # gera áudio
-    generator = pipeline(text, voice="af_heart")
+    # chama API IA
+    response = requests.post(AI_API_URL, json={"text": text}, timeout=120)
 
-    output_path = os.path.join(app.config["GENERATED_FOLDER"], "output.wav")
+    if response.status_code == 200:
 
-    for i, (gs, ps, audio) in enumerate(generator):
-        sf.write(output_path, audio, 24000)
+        audio_filename = f"{uuid.uuid4()}.wav"
 
-    return send_file(output_path, as_attachment=True)
+        output_path = os.path.join(app.config["GENERATED_FOLDER"], audio_filename)
+
+        with open(output_path, "wb") as f:
+            f.write(response.content)
+
+        return send_file(output_path, as_attachment=True)
+
+    return f"Error generating audio: {response.text}"
 
 
 if __name__ == "__main__":
